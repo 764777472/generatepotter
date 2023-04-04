8a469963ea5ddbcf06a532d57d8922721a165bdc
Hhb <33392861+764777472@users.noreply.github.com>
2023-04-04 11:01:36 +0800

    Update generate.ts

--- pages/api/generate.ts
+++ pages/api/generate.ts
@@ -1,7 +1,7 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { Leap } from "@leap-ai/sdk";
 
-const MODEL_ID = process.env.LEAP_MODEL_ID as string;;
+const MODEL_ID = process.env.LEAP_MODEL_ID as string;
 const IMAGE_WIDTH = 600;
 const IMAGE_HEIGHT = 400;
 
